babeccda788e0baa18dd583c4b57d871455aeac2
Jacek <jacek.lajdecki@axonivy.com>
2021-04-22 10:57:44 +0200

    XIVYWF-101/100 Fired signals, signals start

--- workflow-ui/src/ch/ivyteam/workflowui/SignalsBean.java
+++ workflow-ui/src/ch/ivyteam/workflowui/SignalsBean.java
@@ -2,6 +2,7 @@ package ch.ivyteam.workflowui;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -16,6 +17,7 @@ import ch.ivyteam.ivy.workflow.IWorkflowSession;
 import ch.ivyteam.ivy.workflow.query.SignalEventQuery;
 import ch.ivyteam.ivy.workflow.signal.IBpmSignalService;
 import ch.ivyteam.ivy.workflow.signal.ISignalEvent;
+import ch.ivyteam.ivy.workflow.signal.IStartSignalEventElement;
 
 @ManagedBean
 @ViewScoped
@@ -23,7 +25,6 @@ public class SignalsBean
 {
   private String code;
   private String payload;
-
 
   public void sendSignal()
   {
@@ -38,19 +39,31 @@ public class SignalsBean
     {
       signals.send(signalCode, payload);
     }
-    showMessage("Signal "+code+" sent");
+    showMessage("Signal " + code + " sent");
   }
 
   private void showMessage(String msg)
   {
     FacesContext.getCurrentInstance().addMessage(null,
-            new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", msg));
+        new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", msg));
   }
 
   public List<ISignalEvent> getFiredSignals()
   {
-    SignalEventQuery query = IWorkflowContext.current().signals().history().createSignalEventQuery();
-    return new ArrayList<ISignalEvent>(query.executor().results());
+    SignalEventQuery query = IWorkflowContext.current().signals().history().createSignalEventQuery().orderBy().sentTimestamp().descending();
+    return new ArrayList<>(query.executor().results());
+  }
+
+  public List<String> signalsComplete(String query)
+  {
+    String queryLowerCase = query.toLowerCase();
+    List<String> startsList = new ArrayList<>();
+    List<IStartSignalEventElement> starts = IWorkflowContext.current().signals().receivers().all();
+    for (IStartSignalEventElement start : starts)
+    {
+      startsList.add(start.getName());
+    }
+    return startsList.stream().filter(s -> s.toLowerCase().contains(queryLowerCase)).collect(Collectors.toList());
   }
 
   public String getCode()
