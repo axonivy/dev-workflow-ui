8e6e654ba0711d04689af84c125ffa5a1eecbb3c
Jacek <jacek.lajdecki@axonivy.com>
2021-09-10 14:03:01 +0200

    Improve searching for last starts

--- workflow-ui/src/ch/ivyteam/workflowui/homepage/LastStartsModel.java
+++ workflow-ui/src/ch/ivyteam/workflowui/homepage/LastStartsModel.java
@@ -3,12 +3,15 @@ package ch.ivyteam.workflowui.homepage;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.application.IProcessModel;
 import ch.ivyteam.ivy.security.ISession;
 import ch.ivyteam.ivy.workflow.ICase;
+import ch.ivyteam.ivy.workflow.ITask;
 import ch.ivyteam.ivy.workflow.IWorkflowProcessModelVersion;
+import ch.ivyteam.ivy.workflow.IWorkflowSession;
 import ch.ivyteam.ivy.workflow.query.CaseQuery;
 import ch.ivyteam.workflowui.starts.StartableModel;
 
@@ -16,11 +19,11 @@ public class LastStartsModel {
   private List<StartableModel> starts;
 
   public LastStartsModel() {
-    var lastRelativStartLinks = getLastStartedCasesOfUser().stream()
+    var lastRelativStartLinks = getLastStartedCasesOfUser()
             .map(c -> c.getProcessStart().getLink().getRelative()).distinct().collect(Collectors.toList());
 
     starts = getAllStarts().stream()
-            .filter(start -> lastRelativStartLinks.contains(start.getLink().getRelative())).limit(5)
+            .filter(start -> lastRelativStartLinks.contains(start.getLink().getRelative())).limit(10)
             .collect(Collectors.toList());
   }
 
@@ -33,11 +36,14 @@ public class LastStartsModel {
             .collect(Collectors.toList());
   }
 
-  private static List<ICase> getLastStartedCasesOfUser() {
+  private static Stream<ICase> getLastStartedCasesOfUser() {
+    var transientCases = IWorkflowSession.current().findCreatedAndResumedWorkTasks(0, 50).stream()
+            .map(ITask::getCase).map(ICase::getBusinessCase);
+
     var casequery = CaseQuery.create().where().isBusinessCase()
             .and(CaseQuery.create().where().currentUserHasStarted().or().currentUserIsOwner())
             .orderBy().startTimestamp().descending();
-    return casequery.executor().results(0, 500);
+    return Stream.concat(transientCases, casequery.executor().results(0, 500).stream());
   }
 
   public List<StartableModel> getStarts() {
