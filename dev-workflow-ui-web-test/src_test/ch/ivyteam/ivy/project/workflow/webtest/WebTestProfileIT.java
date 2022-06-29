b43248a5cf6b621149cdf90882e3a305c8662fb0
Alexander Suter <alexander.suter@axonivy.com>
2022-06-29 11:18:10 +0200

    XIVY-7376 Add web test for profile view

---
+++ dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/WebTestProfileIT.java
@@ -0,0 +1,50 @@
+package ch.ivyteam.ivy.project.workflow.webtest;
+
+import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.loginFromTable;
+import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.openView;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import com.axonivy.ivy.webtest.IvyWebTest;
+import com.axonivy.ivy.webtest.primeui.PrimeUi;
+import com.codeborne.selenide.Selenide;
+
+@IvyWebTest
+class WebTestProfileIT {
+
+  @Test
+  void profile() {
+    loginFromTable("testuser");
+    openView("profile.xhtml");
+    $("#profileForm\\:userName").shouldBe(exactText("testuser"));
+    $("#profileForm\\:fullName").shouldBe(exactText("testuser"));
+    $("#profileForm\\:email").shouldBe(empty);
+    $("#profileForm\\:roles").shouldBe(exactText("Everybody, testrole1, testrole2, testrole3"));
+
+    var contentLanguage = PrimeUi.selectOne(By.id("profileForm:contentLanguage"));
+    var formattingLanguage = PrimeUi.selectOne(By.id("profileForm:formattingLanguage"));
+
+    contentLanguage.selectItemByLabel("");
+    formattingLanguage.selectItemByLabel("");
+
+    $("#profileForm\\:saveBtn").click();
+    Selenide.refresh();
+
+    contentLanguage.selectedItemShould(empty);
+    contentLanguage.selectItemByValue("de");
+
+    formattingLanguage.selectedItemShould(empty);
+    formattingLanguage.selectItemByValue("de_CH");
+
+    $("#profileForm\\:saveBtn").click();
+    Selenide.refresh();
+
+    contentLanguage.selectedItemShould(text("Deutsch (de)"));
+    formattingLanguage.selectedItemShould(text("Deutsch (Schweiz) (de_CH)"));
+  }
+}
