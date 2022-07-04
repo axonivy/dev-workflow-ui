fd1c073710520f650aa53b6fbffb5860cfa9b579
Reto Weiss <reto.weiss@axonivy.com>
2022-07-04 17:39:53 +0200

    XIVY-8838 Use custom field meta information to display custom fields

--- dev-workflow-ui/src/ch/ivyteam/workflowui/customfield/CustomFieldModel.java
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/customfield/CustomFieldModel.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 import ch.ivyteam.ivy.workflow.ICase;
 import ch.ivyteam.ivy.workflow.ITask;
 import ch.ivyteam.ivy.workflow.custom.field.ICustomField;
+import ch.ivyteam.workflowui.util.EngineModeUtil;
 
 public class CustomFieldModel {
 
   private final String name;
   private final String value;
   private final String type;
+  private final String label;
+  private final String description;
 
   public static List<CustomFieldModel> create(ICase selectedCase) {
     return selectedCase.customFields().all().stream()
@@ -20,6 +23,7 @@ public class CustomFieldModel {
 
   public static List<CustomFieldModel> create(ITask task) {
     return task.customFields().all().stream()
+            .filter(CustomFieldModel::designerOrNotHidden)
             .map(CustomFieldModel::new).collect(Collectors.toList());
   }
 
@@ -27,6 +31,8 @@ public class CustomFieldModel {
     this.name = field.name();
     this.value = field.get().map(Object::toString).orElse("<null>");
     this.type = field.type().toString();
+    this.label = field.meta().label();
+    this.description = field.meta().description();
   }
 
   public String getName() {
@@ -39,5 +45,18 @@ public class CustomFieldModel {
 
   public String getType() {
     return type;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  private static boolean designerOrNotHidden(ICustomField<?> customField) {
+    return EngineModeUtil.isDesigner() ||
+        ! customField.meta().isHidden();
   }
 }
