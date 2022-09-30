dd00086f1c7a2872be844c2d55ed1c268249fb4a
Jacek <jacek.lajdecki@axonivy.com>
2022-09-30 11:23:59 +0200

    Refactor and try fix signals web tests

--- dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/WebTestSignalsIT.java
+++ dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/WebTestSignalsIT.java
@@ -10,6 +10,8 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -19,9 +21,20 @@ import com.axonivy.ivy.webtest.primeui.widget.Table;
 
 @IvyWebTest
 public class WebTestSignalsIT {
+
+  @BeforeAll
+  static void setup() {
+    startTestProcess("175461E47A870BF8/makeAdminUser.ivp");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    loginDeveloper();
+    openView("signals.xhtml");
+  }
+
   @Test
   public void testSignalAdminOnly() {
-    startTestProcess("175461E47A870BF8/makeAdminUser.ivp");
     loginFromTable("testuser");
     openView("signals.xhtml");
     $(By.id("menuform:sr_home")).shouldHave(cssClass("active-menuitem"));
@@ -33,11 +46,6 @@ public class WebTestSignalsIT {
 
   @Test
   public void testSendSignals() {
-    startTestProcess("175461E47A870BF8/makeAdminUser.ivp");
-    loginDeveloper();
-
-    openView("signals.xhtml");
-
     $(By.id("signalForm:signalCodeInput_input")).sendKeys("Web Test Signal");
     $(By.id("signalForm:signalBtn")).shouldBe(enabled).click();
     $(By.id("signalForm:growl_container")).shouldBe(visible);
@@ -48,7 +56,6 @@ public class WebTestSignalsIT {
 
   @Test
   public void testSignalAutocomplete() {
-    openView("signals.xhtml");
     $(By.id("signalForm:signalCodeInput")).findElement(By.tagName("button")).click();
 
     $(By.id("signalForm:signalCodeInput_input")).sendKeys("signal");
@@ -62,7 +69,7 @@ public class WebTestSignalsIT {
 
   @Test
   public void caseProcessViewerCanOpen() {
-    openView("signals.xhtml");
+    $(By.id("signalForm")).shouldBe(visible);
     $(By.id("signalForm:signalCodeInput")).findElement(By.tagName("button")).click();
 
     $(By.id("signalForm:signalCodeInput_input")).sendKeys("test:signal:complete");
@@ -80,10 +87,6 @@ public class WebTestSignalsIT {
 
   @Test
   public void testBoundarySignals() {
-    startTestProcess("175461E47A870BF8/makeAdminUser.ivp");
-    loginDeveloper();
-
-    openView("signals.xhtml");
     Table boundaryTable = PrimeUi.table(By.id("boundarySignalsTable"));
     boundaryTable.contains("No records found.");
 
@@ -98,10 +101,6 @@ public class WebTestSignalsIT {
 
   @Test
   public void testBoundarySignalsOnClick() {
-    startTestProcess("175461E47A870BF8/makeAdminUser.ivp");
-    loginDeveloper();
-
-    openView("signals.xhtml");
     Table boundaryTable = PrimeUi.table(By.id("boundarySignalsTable"));
     boundaryTable.contains("No records found.");
 
