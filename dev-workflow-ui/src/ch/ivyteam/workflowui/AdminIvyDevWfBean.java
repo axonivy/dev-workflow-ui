0c3e3a5d1a8daf77ed01e1fa0dd1ecdab807ad92
Alex <alexander.suter@axonivy.com>
2022-12-05 08:59:20 +0100

    XIVY-10146 Fix warning by removing all unused code in AdminIvyDevWfBean (#152)

--- dev-workflow-ui/src/ch/ivyteam/workflowui/AdminIvyDevWfBean.java
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/AdminIvyDevWfBean.java
@@ -27,55 +27,7 @@ public class AdminIvyDevWfBean {
     return hasPermission(IPermission.WORKFLOW_EVENT_READ_ALL);
   }
 
-  public Boolean hasReadAbsencesPermission() {
-    return hasPermission(IPermission.USER_READ_ABSENCES);
-  }
-
-  public Boolean hasAbsencePermission() {
-    return hasPermission(
-            IPermission.USER_CREATE_ABSENCE,
-            IPermission.USER_READ_ABSENCES);
-  }
-
-  public Boolean hasAnyAbsencePermission() {
-    return hasAtLeastOnePermission(
-            IPermission.USER_CREATE_ABSENCE,
-            IPermission.USER_READ_ABSENCES,
-            IPermission.USER_CREATE_OWN_ABSENCE,
-            IPermission.USER_READ_OWN_ABSENCES);
-  }
-
-  public Boolean hasSubstitutePermission() {
-    return hasPermission(
-            IPermission.USER_CREATE_SUBSTITUTE,
-            IPermission.USER_READ_SUBSTITUTIONS);
-  }
-
-  public Boolean hasAnySubstitutePermission() {
-    return hasAtLeastOnePermission(
-            IPermission.USER_CREATE_SUBSTITUTE,
-            IPermission.USER_READ_SUBSTITUTIONS,
-            IPermission.USER_CREATE_OWN_SUBSTITUTE,
-            IPermission.USER_READ_OWN_SUBSTITUTIONS);
-  }
-
-  public Boolean hasSessionReadAllPermission() {
-    return hasPermission(
-            IPermission.SESSION_READ_ALL,
-            IPermission.SESSION_READ_SESSION_USER,
-            IPermission.SESSION_READ_ACTIVE_SUBSTITUTIONS,
-            IPermission.SESSION_READ_ABSENT);
-  }
-
   private static Boolean hasPermission(IPermission... permissions) {
-    var session = IWorkflowSession.current();
-    var securityDescriptor = ISecurityContext.current().securityDescriptor();
-    return Arrays
-            .stream(permissions)
-            .anyMatch(p -> session.hasPermission(securityDescriptor, p));
-  }
-
-  private static Boolean hasAtLeastOnePermission(IPermission... permissions) {
     var session = IWorkflowSession.current();
     var securityDescriptor = ISecurityContext.current().securityDescriptor();
     return Arrays
