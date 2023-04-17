07458069f9cf4e358b9391af0af10fe358440ce6
Lukas Lieb <lukas.lieb@axonivy.com>
2023-04-17 14:56:20 +0200

    XIVY-11208 Fix task search
    - Tasks: Don't show other states when filtering
    - General: Don't show tasks which are not for the current user
    
    - Don't do some page link check for taskmodel generation

---
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/tasks/PersonalTasksIvyDevWfBean.java
@@ -0,0 +1,31 @@
+package ch.ivyteam.workflowui.tasks;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.SelectEvent;
+
+import ch.ivyteam.workflowui.util.TaskUtil;
+
+@ManagedBean
+@ViewScoped
+public class PersonalTasksIvyDevWfBean {
+  private TasksDataModel tasksDataModel;
+
+  public PersonalTasksIvyDevWfBean() {
+    tasksDataModel = new TasksDataModel(true);
+    tasksDataModel.setFilter("");
+  }
+
+  public TasksDataModel getTasksDataModel() {
+    return tasksDataModel;
+  }
+
+  public void executeTaskRow(SelectEvent<?> event) {
+    TaskUtil.executeTaskRow(event);
+  }
+
+  public void executeTask(long taskId) {
+   TaskUtil.executeTask(taskId);
+  }
+}
