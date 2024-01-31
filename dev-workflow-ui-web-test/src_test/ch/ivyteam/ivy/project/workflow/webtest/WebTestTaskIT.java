1761f8c93d542bc9b7e746dcebbcef19d2a1bc4f
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-01-31 08:38:26 +0100

    XIVY-6821 Fixed TaskDetailPage TestCase

--- dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/WebTestTaskIT.java
+++ dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/WebTestTaskIT.java
@@ -1,56 +1,43 @@
 package ch.ivyteam.ivy.project.workflow.webtest;
 
+import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.loginDeveloper;
+import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.openView;
+import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.startTestProcess;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogType;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.primeui.PrimeUi;
-import com.axonivy.ivy.webtest.primeui.widget.Table;
 import com.codeborne.selenide.Selenide;
-
-import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.loginDeveloper;
-import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.startTestProcess;
-import static com.codeborne.selenide.Condition.text;
-import static ch.ivyteam.ivy.project.workflow.webtest.util.WorkflowUiUtil.openView;
-import static com.codeborne.selenide.Selenide.$;
 
 @IvyWebTest
 public class WebTestTaskIT {
-		
-	@BeforeAll
-	public static void setup() {
-		loginDeveloper();
-		startTestProcess("18D3AB6E2DC7779B/generateUserTask.ivp");
-		openView("tasks.xhtml");
-	    var table = PrimeUi.table(By.id("tasksForm:tasks"));
-	    table.row(0).shouldHave(text("TaskDetailTest"));
-	    
-	    var element = $(By.id("tasksForm:tasks:0:taskState"));
-	    assertThat(element.getAttribute("class")).contains("task-state-open");
-	    
-	    element.click();
-	    
-	}
-	
-	@Test
-	public void testDetailPage() {
-		loginDeveloper();
-		openView("tasks.xhtml");
-		
-		var element = $(By.id("tasksForm:tasks:0:taskState"));
-		assertThat(element.getAttribute("class")).contains("task-state-in-progress");
-		
-		element.click();
-		
-		System.out.println($(By.tagName("body")).innerHtml());
-		System.out.println(Selenide.webdriver().driver().url());
-		assertThat($(By.className("layout-dashboard")).exists()).isTrue();
-		
-	}
 
+  @BeforeAll
+  public static void setup() {
+    loginDeveloper();
+    startTestProcess("18D3AB6E2DC7779B/generateUserTask.ivp");
+    openView("tasks.xhtml");
+    var table = PrimeUi.table(By.id("tasksForm:tasks"));
+    table.row(0).shouldHave(text("TaskDetailTest"));
+    var element = $(By.id("tasksForm:tasks:0:taskState"));
+    assertThat(element.getAttribute("class")).contains("task-state-open");
+    element.click();
+  }
+
+  @Test
+  public void testDetailPage() {
+    Selenide.closeWebDriver();
+    loginDeveloper();
+    openView("tasks.xhtml");
+    var element = $(By.id("tasksForm:tasks:0:taskState"));
+    assertThat(element.getAttribute("class")).contains("task-state-in-progress");
+    element.click();
+    assertThat($(By.className("layout-dashboard")).getLocation() != null).isTrue();
+  }
 }
