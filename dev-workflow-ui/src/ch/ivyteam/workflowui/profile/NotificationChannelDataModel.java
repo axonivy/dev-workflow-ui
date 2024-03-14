de9f890b7d7b44da923f7ad0841cb5015ad5478d
Reto Weiss <reto.weiss@axonivy.com>
2024-03-14 09:56:00 +0100

    XIVY-13639 Add notification subscription to user profile page
    Add a notification subscription section to the user profile page
    
    Implement new standard process to navigate to the notification
    subscription settings page of a user

---
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/profile/NotificationChannelDataModel.java
@@ -0,0 +1,102 @@
+package ch.ivyteam.workflowui.profile;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import ch.ivyteam.ivy.notification.channel.Event;
+import ch.ivyteam.ivy.notification.channel.NotificationSubscription;
+import ch.ivyteam.ivy.security.ISecurityContext;
+import ch.ivyteam.ivy.security.ISecurityMember;
+
+public class NotificationChannelDataModel {
+
+  private final ISecurityMember subscriber;
+  private final ISecurityContext securityContext;
+
+  private List<NotificationEventDto> events;
+  private List<NotificationChannelDto> channels;
+
+  private NotificationChannelDataModel(ISecurityMember subscriber) {
+    this.subscriber = subscriber;
+    this.securityContext = subscriber.getSecurityContext();
+  }
+
+  public void onload() {
+    events = NotificationEventDto.all();
+    channels = NotificationChannelDto.all(subscriber, securityContext);
+  }
+
+  public void reset() {
+    channels.forEach(this::resetChannel);
+    onload();
+    addMessage("Notification Channels reset");
+  }
+
+  private void resetChannel(NotificationChannelDto channel) {
+    channel.getSubscriptions().forEach(
+            (event, subscription) -> subscription.setState(NotificationChannelSubscriptionDto.State.USE_DEFAULT));
+    saveChannel(channel);
+  }
+
+  public void save() {
+    channels.forEach(this::saveChannel);
+    addMessage("Notification Channels changes saved");
+  }
+
+  private void saveChannel(NotificationChannelDto channel) {
+    channel.getSubscriptions().entrySet().forEach(eventSubscription -> {
+      var subscription = NotificationSubscription.of(subscriber, channel.getChannel(), eventSubscription.getKey());
+      subscription.state(eventSubscription.getValue().getState().toDbState());
+    });
+  }
+
+  private void addMessage(String msg) {
+    FacesContext.getCurrentInstance().addMessage("notificationMessage", new FacesMessage(msg));
+  }
+
+  public List<NotificationChannelDto> getChannels() {
+    return channels;
+  }
+
+  public List<NotificationEventDto> getEvents() {
+    return events;
+  }
+
+  public static NotificationChannelDataModel instance(ISecurityMember member) {
+    var model = new NotificationChannelDataModel(member);
+    model.onload();
+    return model;
+  }
+
+  public static final class NotificationEventDto {
+
+    private Event event;
+
+    private NotificationEventDto(Event event) {
+      this.event = event;
+    }
+
+    public Event getEvent() {
+      return event;
+    }
+
+    public String getDisplayName() {
+      return event.displayName(Locale.ENGLISH);
+    }
+
+    public String getDescription() {
+      return event.description(Locale.ENGLISH);
+    }
+
+    public static List<NotificationEventDto> all() {
+      return Event.all().stream().map(NotificationEventDto::new).toList();
+    }
+
+    public static NotificationEventDto of(Event event) {
+      return new NotificationEventDto(event);
+    }
+  }
+}
---
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/profile/NotificationChannelDto.java
@@ -0,0 +1,83 @@
+package ch.ivyteam.workflowui.profile;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import ch.ivyteam.ivy.notification.channel.Event;
+import ch.ivyteam.ivy.notification.channel.NotificationChannel;
+import ch.ivyteam.ivy.notification.channel.NotificationSubscription;
+import ch.ivyteam.ivy.security.ISecurityContext;
+import ch.ivyteam.ivy.security.ISecurityMember;
+import ch.ivyteam.workflowui.profile.NotificationChannelDataModel.NotificationEventDto;
+
+public class NotificationChannelDto {
+
+  private final NotificationChannel channel;
+  private final Map<Event, NotificationChannelSubscriptionDto> subscriptions;
+
+  private NotificationChannelDto(NotificationChannel channel,
+          Map<Event, NotificationChannelSubscriptionDto> subscriptions) {
+    this.channel = channel;
+    this.subscriptions = subscriptions;
+  }
+
+  public static List<NotificationChannelDto> all(ISecurityMember subscriber, ISecurityContext securityContext) {
+    var channels = NotificationChannel.all(securityContext).stream()
+            .filter(channel -> channel.config().enabled())
+            .map(channel -> toChannel(subscriber, channel))
+            .toList();
+    channels.forEach(channel -> Event.all().forEach(event -> channel.setSubscriptionIconAndTitle(NotificationEventDto.of(event))));
+    return channels;
+  }
+
+  private static NotificationChannelDto toChannel(ISecurityMember subscriber, NotificationChannel channel) {
+    var subscriptions = channel.configFor(subscriber).subscriptions().stream()
+            .collect(Collectors.toMap(
+                    NotificationSubscription::event,
+                    subscription -> new NotificationChannelSubscriptionDto(
+                            subscription.state(),
+                            subscription.isSubscribedByDefault())));
+    return new NotificationChannelDto(channel, subscriptions);
+  }
+
+  public NotificationChannel getChannel() {
+    return channel;
+  }
+
+  public Map<Event, NotificationChannelSubscriptionDto> getSubscriptions() {
+    return subscriptions;
+  }
+
+  public NotificationChannelSubscriptionDto getSubscription(NotificationEventDto event) {
+    return subscriptions.get(event.getEvent());
+  }
+
+  public void setSubscriptionIconAndTitle(NotificationEventDto event) {
+    var subscription = subscriptions.get(event.getEvent());
+    var state = subscription.getState();
+
+    boolean subscribedByUser = state.equals(NotificationChannelSubscriptionDto.State.SUBSCRIBED);
+
+    boolean useDefault = state.equals(NotificationChannelSubscriptionDto.State.USE_DEFAULT);
+
+    var icon = new StringBuilder();
+    var iconTitle = new StringBuilder();
+
+    if (subscribedByUser || (useDefault && subscription.isSubscribedByDefault())) {
+      icon.append("check-circle-1 state-active");
+      iconTitle.append("Subscribed");
+    } else {
+      icon.append("remove-circle state-inactive");
+      iconTitle.append("Not subscribed");
+    }
+
+    if (useDefault) {
+      icon.append(" light");
+      iconTitle.append(" by default");
+    }
+
+    subscription.setIcon(icon.toString());
+    subscription.setTitle(iconTitle.toString());
+  }
+ }
