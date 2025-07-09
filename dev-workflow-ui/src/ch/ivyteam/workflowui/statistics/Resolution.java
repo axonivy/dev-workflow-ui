e90c763c59c83231152936167f02bcf5de5d81bb
Jacek <jacek.lajdecki@axonivy.com>
2025-07-09 15:17:09 +0200

    XIVY-14746 Add Statistics graphs resolution selection

---
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/statistics/Resolution.java
@@ -0,0 +1,9 @@
+package ch.ivyteam.workflowui.statistics;
+
+public interface Resolution {
+  String HOUR = "hour";
+  String HOUR6 = "hour6";
+  String DAY = "day";
+  String WEEK = "week";
+  String MONTH = "month";
+}
---
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/statistics/TimeDuration.java
@@ -0,0 +1,11 @@
+package ch.ivyteam.workflowui.statistics;
+
+public interface TimeDuration {
+  String TODAY = "today";
+  String LAST_24H = "24h";
+  String LAST_7D = "7d/d";
+  String LAST_30D = "30d/d";
+  String LAST_90D = "90d/d";
+  String LAST_365D = "365d/d";
+  String ALL_TIME = "all";
+}
