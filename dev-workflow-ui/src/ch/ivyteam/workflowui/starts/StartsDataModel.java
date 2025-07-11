5828e9bb9eafea359eaa319d227d99c50be61b4a
Jacek <jacek.lajdecki@axonivy.com>
2025-07-11 15:53:25 +0200

    XIVY-15890 Add projects filter button

--- dev-workflow-ui/src/ch/ivyteam/workflowui/starts/StartsDataModel.java
+++ dev-workflow-ui/src/ch/ivyteam/workflowui/starts/StartsDataModel.java
@@ -1,32 +1,43 @@
 package ch.ivyteam.workflowui.starts;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ch.ivyteam.workflowui.util.ProcessModelsUtil;
 
 public class StartsDataModel {
 
-  private final List<StartableModel> startables;
-  private String filter;
+  private final List<StartableModel> originalStartables;
+  private List<StartableModel> startables;
+  private String globalFilter = "";
   private final boolean isOnlySingleApplication;
   private final boolean isOnlySingleProject;
+  private final ProjectFilterModel projectFilterModel;
 
   public StartsDataModel() {
-    startables = ProcessModelsUtil.getDeployedStartables();
+    originalStartables = ProcessModelsUtil.getDeployedStartables();
+    startables = new ArrayList<>(originalStartables);
     isOnlySingleApplication = startables.stream().map(StartableModel::getApplicationName).distinct().count() == 1;
     isOnlySingleProject = startables.stream().map(StartableModel::getProjectName).distinct().count() == 1;
+    var allProjects = startables.stream().map(StartableModel::getProjectName).distinct().sorted().toList();
+    projectFilterModel = new ProjectFilterModel(allProjects);
   }
 
   public List<StartableModel> getStartables() {
     return startables;
   }
 
-  public void setFilter(String query) {
-    filter = query;
+  public String getGlobalFilter() {
+    return globalFilter;
   }
 
-  public String getFilter() {
-    return filter;
+  public void setGlobalFilter(String globalFilter) {
+    this.globalFilter = globalFilter;
+  }
+
+  public void resetGlobalFilter() {
+    this.globalFilter = "";
   }
 
   public boolean isOnlySingleApplication() {
@@ -37,4 +48,18 @@ public class StartsDataModel {
     return isOnlySingleProject;
   }
 
+  public ProjectFilterModel getProjectFilterModel() {
+    return projectFilterModel;
+  }
+
+  public void applyProjectFilter() {
+    this.startables = originalStartables.stream()
+        .filter(start -> this.projectFilterModel.getAppliedProjects().contains(start.getProjectName()))
+        .collect(Collectors.toList());
+  }
+
+  public void resetAllProjectFilters() {
+    projectFilterModel.resetAll();
+    applyProjectFilter();
+  }
 }
