c2def4ce16a2e9bfed9aafead692bca62f5162a8
Alexander Suter <alexander.suter@axonivy.com>
2026-09-09 15:27:25 +0200

    Sonar: Unused assignments should be removed

--- dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/util/WorkflowUiUtil.java
+++ dev-workflow-ui-web-test/src_test/ch/ivyteam/ivy/project/workflow/webtest/util/WorkflowUiUtil.java
@@ -140,7 +140,8 @@ public class WorkflowUiUtil {
     } catch (TimeoutException _) {
       System.out.println("Browser didn't respond in time, retry: " + retry);
       Selenide.closeWebDriver();
-      open(url, retry++);
+      retry = retry + 1;
+      open(url, retry);
     }
   }
 }
